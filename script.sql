05506deb6ddb3dcee611a047e82f0dc6d89e7767
rrizaini <rizaini.ramlie@time.commy>
2023-04-17 10:53:22 +0800

    improved query

--- script.sql
+++ script.sql
@@ -1,118 +1,54 @@
-SELECT Jobs.id AS `Jobs__id`,
-Jobs.name AS `Jobs__name`,
-Jobs.media_id AS `Jobs__media_id`,
-Jobs.job_category_id AS `Jobs__job_category_id`,
-Jobs.job_type_id AS `Jobs__job_type_id`,
-Jobs.description AS `Jobs__description`,
-Jobs.detail AS `Jobs__detail`,
-Jobs.business_skill AS `Jobs__business_skill`,
-Jobs.knowledge AS `Jobs__knowledge`,
-Jobs.location AS `Jobs__location`,
-Jobs.activity AS `Jobs__activity`,
-Jobs.academic_degree_doctor AS `Jobs__academic_degree_doctor`,
-Jobs.academic_degree_master AS `Jobs__academic_degree_master`,
-Jobs.academic_degree_professional AS `Jobs__academic_degree_professional`,
-Jobs.academic_degree_bachelor AS `Jobs__academic_degree_bachelor`,
-Jobs.salary_statistic_group AS `Jobs__salary_statistic_group`,
-Jobs.salary_range_first_year AS `Jobs__salary_range_first_year`,
-Jobs.salary_range_average AS `Jobs__salary_range_average`,
-Jobs.salary_range_remarks AS `Jobs__salary_range_remarks`,
-Jobs.restriction AS `Jobs__restriction`,
-Jobs.estimated_total_workers AS `Jobs__estimated_total_workers`,
-Jobs.remarks AS `Jobs__remarks`,
-Jobs.url AS `Jobs__url`,
-Jobs.seo_description AS `Jobs__seo_description`,
-Jobs.seo_keywords AS `Jobs__seo_keywords`,
-Jobs.sort_order AS `Jobs__sort_order`,
-Jobs.publish_status AS `Jobs__publish_status`,
-Jobs.version AS `Jobs__version`,
-Jobs.created_by AS `Jobs__created_by`,
-Jobs.created AS `Jobs__created`,
-Jobs.modified AS `Jobs__modified`,
-Jobs.deleted AS `Jobs__deleted`,
-JobCategories.id AS `JobCategories__id`,
-JobCategories.name AS `JobCategories__name`,
-JobCategories.sort_order AS `JobCategories__sort_order`,
-JobCategories.created_by AS `JobCategories__created_by`,
-JobCategories.created AS `JobCategories__created`,
-JobCategories.modified AS `JobCategories__modified`,
-JobCategories.deleted AS `JobCategories__deleted`,
-JobTypes.id AS `JobTypes__id`,
-JobTypes.name AS `JobTypes__name`,
-JobTypes.job_category_id AS `JobTypes__job_category_id`,
-JobTypes.sort_order AS `JobTypes__sort_order`,
-JobTypes.created_by AS `JobTypes__created_by`,
-JobTypes.created AS `JobTypes__created`,
-JobTypes.modified AS `JobTypes__modified`,
-JobTypes.deleted AS `JobTypes__deleted`
-FROM jobs Jobs
-LEFT JOIN jobs_personalities JobsPersonalities
-ON Jobs.id = (JobsPersonalities.job_id)
-LEFT JOIN personalities Personalities
-ON (Personalities.id = (JobsPersonalities.personality_id)
-AND (Personalities.deleted) IS NULL)
-LEFT JOIN jobs_practical_skills JobsPracticalSkills
-ON Jobs.id = (JobsPracticalSkills.job_id)
-LEFT JOIN practical_skills PracticalSkills
-ON (PracticalSkills.id = (JobsPracticalSkills.practical_skill_id)
-AND (PracticalSkills.deleted) IS NULL)
-LEFT JOIN jobs_basic_abilities JobsBasicAbilities
-ON Jobs.id = (JobsBasicAbilities.job_id)
-LEFT JOIN basic_abilities BasicAbilities
-ON (BasicAbilities.id = (JobsBasicAbilities.basic_ability_id)
-AND (BasicAbilities.deleted) IS NULL)
-LEFT JOIN jobs_tools JobsTools
-ON Jobs.id = (JobsTools.job_id)
-LEFT JOIN affiliates Tools
-ON (Tools.type = 1
-AND Tools.id = (JobsTools.affiliate_id)
-AND (Tools.deleted) IS NULL)
-LEFT JOIN jobs_career_paths JobsCareerPaths
-ON Jobs.id = (JobsCareerPaths.job_id)
-LEFT JOIN affiliates CareerPaths
-ON (CareerPaths.type = 3
-AND CareerPaths.id = (JobsCareerPaths.affiliate_id)
-AND (CareerPaths.deleted) IS NULL)
-LEFT JOIN jobs_rec_qualifications JobsRecQualifications
-ON Jobs.id = (JobsRecQualifications.job_id)
-LEFT JOIN affiliates RecQualifications
-ON (RecQualifications.type = 2
-AND RecQualifications.id = (JobsRecQualifications.affiliate_id)
-AND (RecQualifications.deleted) IS NULL)
-LEFT JOIN jobs_req_qualifications JobsReqQualifications
-ON Jobs.id = (JobsReqQualifications.job_id)
-LEFT JOIN affiliates ReqQualifications
-ON (ReqQualifications.type = 2
-AND ReqQualifications.id = (JobsReqQualifications.affiliate_id)
-AND (ReqQualifications.deleted) IS NULL)
-INNER JOIN job_categories JobCategories
-ON (JobCategories.id = (Jobs.job_category_id)
-AND (JobCategories.deleted) IS NULL)
-INNER JOIN job_types JobTypes
-ON (JobTypes.id = (Jobs.job_type_id)
-AND (JobTypes.deleted) IS NULL)
-WHERE ((JobCategories.name LIKE '%キャビンアテンダント%'
-OR JobTypes.name LIKE '%キャビンアテンダント%'
-OR Jobs.name LIKE '%キャビンアテンダント%'
-OR Jobs.description LIKE '%キャビンアテンダント%'
-OR Jobs.detail LIKE '%キャビンアテンダント%'
-OR Jobs.business_skill LIKE '%キャビンアテンダント%'
-OR Jobs.knowledge LIKE '%キャビンアテンダント%'
-OR Jobs.location LIKE '%キャビンアテンダント%'
-OR Jobs.activity LIKE '%キャビンアテンダント%'
-OR Jobs.salary_statistic_group LIKE '%キャビンアテンダント%'
-OR Jobs.salary_range_remarks LIKE '%キャビンアテンダント%'
-OR Jobs.restriction LIKE '%キャビンアテンダント%'
-OR Jobs.remarks LIKE '%キャビンアテンダント%'
-OR Personalities.name LIKE '%キャビンアテンダント%'
-OR PracticalSkills.name LIKE '%キャビンアテンダント%'
-OR BasicAbilities.name LIKE '%キャビンアテンダント%'
-OR Tools.name LIKE '%キャビンアテンダント%'
-OR CareerPaths.name LIKE '%キャビンアテンダント%'
-OR RecQualifications.name LIKE '%キャビンアテンダント%'
-OR ReqQualifications.name LIKE '%キャビンアテンダント%')
-AND publish_status = 1
-AND (Jobs.deleted) IS NULL)
-GROUP BY Jobs.id
-ORDER BY Jobs.sort_order desc,
-Jobs.id DESC LIMIT 50 OFFSET 0;
+SELECT j.id AS id, 
+       j.name AS name, 
+       j.media_id AS media_id, 
+       j.job_category_id AS job_category_id, 
+       j.job_type_id AS job_type_id, 
+       j.description AS description, 
+       j.detail AS detail, 
+       j.business_skill AS business_skill, 
+       j.knowledge AS knowledge, 
+       j.location AS location, 
+       j.activity AS activity, 
+       j.academic_degree_doctor AS academic_degree_doctor, 
+       j.academic_degree_master AS academic_degree_master, 
+       j.academic_degree_professional AS academic_degree_professional, 
+       j.academic_degree_bachelor AS academic_degree_bachelor, 
+       j.salary_statistic_group AS salary_statistic_group, 
+       j.salary_range_first_year AS salary_range_first_year, 
+       j.salary_range_average AS salary_range_average, 
+       j.salary_range_remarks AS salary_range_remarks, 
+       j.restriction AS restriction, 
+       j.estimated_total_workers AS estimated_total_workers, 
+       j.remarks AS remarks, 
+       j.url AS url, 
+       j.seo_description AS seo_description, 
+       j.seo_keywords AS seo_keywords, 
+       j.sort_order AS sort_order, 
+       j.publish_status AS publish_status, 
+       j.version AS version, 
+       j.created_by AS created_by, 
+       j.created AS created, 
+       j.modified AS modified, 
+       j.deleted AS deleted, 
+       jc.id AS job_category_id, 
+       jc.name AS job_category_name, 
+       jc.sort_order AS job_category_sort_order, 
+       jc.created_by AS job_category_created_by, 
+       jc.created AS job_category_created, 
+       jc.modified AS job_category_modified, 
+       jc.deleted AS job_category_deleted, 
+       jt.id AS job_type_id, 
+       jt.name AS job_type_name, 
+       jt.job_category_id AS job_type_category_id, 
+       jt.sort_order AS job_type_sort_order, 
+       jt.created_by AS job_type_created_by, 
+       jt.created AS job_type_created, 
+       jt.modified AS job_type_modified, 
+       jt.deleted AS job_type_deleted 
+FROM jobs j 
+INNER JOIN job_categories jc ON jc.id = j.job_category_id 
+INNER JOIN job_types jt ON jt.id = j.job_type_id 
+WHERE MATCH (j.name, j.description, j.detail, j.business_skill, j.knowledge, j.location, j.activity, j.salary_range_remarks, j.restriction, j.remarks) AGAINST ('キャビンアテンダント' IN BOOLEAN MODE) 
+AND j.deleted = 0 
+AND jc.deleted = 0 
+AND jt.deleted = 0;
